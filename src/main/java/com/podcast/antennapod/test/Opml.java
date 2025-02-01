38e6bab88db6d40030cb8e44891b2ebba24c2320
Samuel Douay <douaysamuel@gmail.com>
2025-02-01 17:40:47 +0100

    test parser xml

--- src/main/java/com/podcast/antennapod/test/Opml.java
+++ src/main/java/com/podcast/antennapod/test/Opml.java
@@ -25,7 +25,39 @@ public class Opml {
 
             for (Iterator<Node> iter = list.iterator(); iter.hasNext();) {
                 DefaultElement attribute = (DefaultElement) iter.next();
-                logger.info(attribute.attribute("xmlUrl").getValue());
+                logger.info( attribute.attribute("text").getValue() +" | " +attribute.attribute("xmlUrl").getValue());
+            }
+
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        try {
+            SAXReader saxReader = new SAXReader();
+            saxReader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            document = saxReader.read("https://feeds.acast.com/public/shows/5e6a404cd22bfc26784b114c");
+
+            List<Node> list = document.selectNodes("//channel/title");
+
+            for (Iterator<Node> iter = list.iterator(); iter.hasNext();) {
+                DefaultElement element = (DefaultElement) iter.next();
+                logger.info(element + " | " + element.getName() + " | " + element.getText());
+            }
+
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        try {
+            SAXReader saxReader = new SAXReader();
+            saxReader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            document = saxReader.read("https://feeds.acast.com/public/shows/5e6a404cd22bfc26784b114c");
+
+            List<Node> list = document.selectNodes("//channel/item/title");
+
+            for (Iterator<Node> iter = list.iterator(); iter.hasNext();) {
+                DefaultElement element = (DefaultElement) iter.next();
+                logger.info(element + " | " + element.getName() + " | " + element.getText());
             }
 
         } catch (Exception e) {
