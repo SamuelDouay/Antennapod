0f779d787440fc6b54fac6a4ff61a099f35c0caf
Samuel DOUAY-RYCKELYNCK <sdouay-ryckelynck@partner.oney.fr>
2025-02-19 09:21:32 +0100

    test toString Item

--- src/test/java/com/podcast/antennapod/util/opml/ItemOpmlTest.java
+++ src/test/java/com/podcast/antennapod/util/opml/ItemOpmlTest.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Item Opml Test")
 public class ItemOpmlTest {
+    final String title = "Test";
+    final String type = "RSS";
+    final String xmlUrl = "Xml";
+    final String htmlUrl = "HTML";
 
     @DisplayName("Test Constructor")
     @Test
     public void testConstructor() {
-        final String title = "Test";
-        final String type = "RSS";
-        final String xmlUrl = "Xml";
-        final String htmlUrl = "HTML";
 
         ItemOpml test = new ItemOpml(title, type, xmlUrl, htmlUrl);
 
@@ -22,5 +22,18 @@ public class ItemOpmlTest {
         assertEquals(type, test.getType());
         assertEquals(xmlUrl, test.getXmlUrl());
         assertEquals(htmlUrl, test.getHtmlUrl());
+    }
+
+    @DisplayName("Test string")
+    @Test
+    public void testString() {
+        ItemOpml test = new ItemOpml(title, type, xmlUrl, htmlUrl);
+
+        assertEquals(test.toString(), "ItemOpml{" +
+                "title='" + title + '\'' +
+                ", type='" + type + '\'' +
+                ", xmlUrl='" + xmlUrl + '\'' +
+                ", htmlUrl='" + htmlUrl + '\'' +
+                '}');
     }
 }
