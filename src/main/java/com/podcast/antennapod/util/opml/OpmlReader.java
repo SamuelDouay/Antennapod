bf2f2ec775ae83e428b6b37a6c9de2babe89a96e
Samuel DOUAY-RYCKELYNCK <sdouay-ryckelynck@partner.oney.fr>
2025-02-19 10:40:42 +0100

    reader

--- src/main/java/com/podcast/antennapod/util/opml/OpmlReader.java
+++ src/main/java/com/podcast/antennapod/util/opml/OpmlReader.java
@@ -7,34 +7,47 @@ import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 import org.dom4j.tree.DefaultElement;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class OpmlReader {
     private static final Logger logger = LogManager.getLogger(OpmlReader.class);
+    private static final String FEATURE = "http://apache.org/xml/features/disallow-doctype-decl";
+    private static final boolean FEATURE_BOOL = true;
+    private static final String OUTLINE = "//body/outline";
 
-    public static void main(String[] args) {
-        Document document;
-
+    public static List<ItemOpml> read() {
         try {
+            List<ItemOpml> itemOpmls = new ArrayList<>();
+
             SAXReader saxReader = new SAXReader();
-            saxReader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-            document = saxReader.read(OpmlReader.class.getResourceAsStream("/tmp/antennapod-feeds-2024-06-29.opml"));
+            saxReader.setFeature(FEATURE, FEATURE_BOOL);
+            Document document = saxReader.read(OpmlReader.class.getResourceAsStream("/tmp/antennapod-feeds-2024-06-29.opml"));
 
-            List<Node> list = document.selectNodes("//body/outline");
+            List<Node> list = document.selectNodes(OUTLINE);
 
-            for (Iterator<Node> iter = list.iterator(); iter.hasNext();) {
-                DefaultElement attribute = (DefaultElement) iter.next();
+            for (Node node : list) {
+                DefaultElement attribute = (DefaultElement) node;
                 String title = attribute.attribute("text").getValue();
                 String type = attribute.attribute("type").getValue();
                 String xmlUrl = attribute.attribute("xmlUrl").getValue();
                 String htmlUrl = attribute.attribute("htmlUrl").getValue();
-                logger.info(new ItemOpml(title, type, xmlUrl, htmlUrl));
+
+
+                itemOpmls.add(new ItemOpml(title, type, xmlUrl, htmlUrl));
             }
+
+            return itemOpmls;
 
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        logger.info(OpmlReader.read());
     }
 
 }
